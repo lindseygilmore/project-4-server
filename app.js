fe424c322c84430d7fd030005ea7121c96fc5504
lindseygilmore <lindseygilmore21@gmail.com>
2017-08-11 14:37:28 -0500

    yelp api rendering

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var path = require('path');
+var axios = require('axios');
+var cors = require('cors');
+
+require('dotenv').config();
 
 var Yelp = require('yelp');
+
 
 var yelp = new Yelp({
 	consumer_key: process.env.CONSUMER_KEY, 
@@ -12,19 +17,24 @@ var yelp = new Yelp({
 	token_secret: process.env.TOKEN_SECRET,
 });
 
-
+app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-yelp.search({ term: 'food', location: 'Chicago' })
-	.then(function(data){
-		console.log(data);
-	})
-	.catch(function(err){
-		console.log(err);
-	});
-
 
 app.get('/eat', function(req, res){
 
+    axios.get("https://api.yelp.com/v3/businesses/search?location=chicago&term=restaurants", {headers: {'Authorization': 'Bearer ' + process.env.TOKEN}} )
+
+    .then(function(response){
+        console.log(response)
+        res.send(response.data)
+     }).catch(function(err){
+     	console.log(err);
+     	res.send(err);
+     })
+	});
+
+
+
+server.listen(3000, function(){
+	console.log("listening on port 3000")
 })
